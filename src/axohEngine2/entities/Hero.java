8ee4174e63b74e0c07ef32158e35d89087dbe53c
Khaled Abu-Ghazaleh <krymtel@gmail.com>
2016-10-26 17:49:17 -0400

    Draw a pixel at the top-left of the viewport.
    If this pixel is NOT AT THE TOP-LEFT OF THE VIEWPORT, then THERE'S
    SOMETHING HORRIBLY WRONG.

--- src/axohEngine2/entities/Hero.java
+++ src/axohEngine2/entities/Hero.java
@@ -1,5 +1,6 @@
 package axohEngine2.entities;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
 
 import axohEngine2.Judgement;
@@ -9,6 +10,13 @@ public class Hero extends Mob {
 
 	public Hero(Judgement game, Graphics2D g2d, SpriteSheet sheet, int spriteNumber, String name) {
 		super(game, g2d, sheet, spriteNumber, TYPE.PLAYER, name);
+	}
+	
+	@Override
+	public void renderMob() {
+		super.renderMob();
+		g2d.setColor(Color.green);
+		g2d.fillRect(0, 0, 1, 1);
 	}
 
 }
