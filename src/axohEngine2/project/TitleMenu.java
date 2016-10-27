768098c143934fe798d0c524371204a4108bbd22
carlyemile <carlyemile@gmail.com>
2016-10-26 20:49:57 -0400

    Controls screen fix

--- src/axohEngine2/project/TitleMenu.java
+++ src/axohEngine2/project/TitleMenu.java
@@ -135,12 +135,13 @@ public class TitleMenu {
 			
 			
 			  g2d.drawString("How to Play", 70, 100);
-			  g2d.setFont(new Font("Arial", Font.PLAIN, 40));
+			  g2d.setFont(new Font("Arial", Font.PLAIN, 30));
 			  
 			  //story
-			  g2d.drawString("You are a professor who is known for their phenomenal elephant jokes that can just make a student's day. Little did you know, the more you told these jokes,", 120, 160);
-			  g2d.drawString("the more this angered the elephants of the world. They have come to seek revenge. You must fight the angry elephants in order to survive. Try to stay alive", 120, 210);
-			  g2d.drawString("for as long as you can!", 120, 260);
+			  g2d.drawString("You are a professor who is known for their phenomenal elephant jokes that can just make a student's day.", 120, 160);
+			  g2d.drawString("Little did you know, the more you told these jokes, the more this angered the elephants of the world.", 120, 200);
+			  g2d.drawString("They have come to seek revenge. You must fight the angry elephants in order to survive. Try to stay alive", 120, 240);
+			  g2d.drawString("for as long as you can!", 120, 280);
 
 			  
 			  
